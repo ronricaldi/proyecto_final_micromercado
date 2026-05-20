aacb77371f0d94248b6d9e747ff3200ca7efa113
Ron <ronald.ricaldi@gmail.com>
2026-05-20 09:45:45 -0400

    Primera fase: usuarios

---
+++ usuario.py
@@ -0,0 +1,28 @@
+#Desarrollo en entrada de inputs
+
+usuario_creacion = input("Cree su usuario, con mínimamente 8 caracteres: ")
+
+if len(usuario_creacion) < 8:
+    print("Su nombre de usuario debe tener mínimamente 8 caracteres")
+else:
+    print("""Usuario creado correctamente 
+          creando usuario:
+          >>>>>>
+          >>>>>>>>>>>>
+          >>>>>>>>>>>>>>>>>>""")
+
+
+usuario = input("Ingrese su usuario: ")
+intentos = 3
+while intentos == 3:
+    usuario_creacion == usuario
+    print("Usuario correcto, procesando.....")
+    break
+if usuario != usuario:
+     print("Su usuario es incorrecto: Importante.- Solo tiene 3 intentos")
+
+
+
+
+
+                         
